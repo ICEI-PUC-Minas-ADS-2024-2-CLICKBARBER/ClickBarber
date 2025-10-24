6e18cd4bd7d7a5d8cb8cb4d8e06f298c0b869454
Leonardo-m-web <leonardo.m.j.soares@gmail.com>
2025-10-24 19:54:57 -0300

    alteração minima na fonte e no tamanho do botão de ocultação de senha

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.use(cors());
 
 app.use(cors({
-  origin: 'http://127.0.0.1:5500', 
+  origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true 
 }));
